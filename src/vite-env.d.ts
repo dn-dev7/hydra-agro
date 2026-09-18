3167990081492ff238bb51c84dd546e2aa877850
dn <vtzinpl7@gmail.com>
2026-09-18 00:04:37 -0300

    chore(ai): type Nivo Agro endpoint

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,6 +5,7 @@ declare const __HYDRA_BUILD__: string;
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL?: string;
   readonly VITE_SUPABASE_PUBLISHABLE_KEY?: string;
+  readonly VITE_NIVO_AGRO_API?: string;
 }
 
 interface ImportMeta {
