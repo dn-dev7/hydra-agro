0f260c0fb6b5c0cc03dd1e6f490e136b4efa9a2f
dn <vtzinpl7@gmail.com>
2026-09-18 00:11:00 -0300

    chore(whatsapp): type bot phone environment value

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,6 +6,7 @@ interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL?: string;
   readonly VITE_SUPABASE_PUBLISHABLE_KEY?: string;
   readonly VITE_NIVO_AGRO_API?: string;
+  readonly VITE_NIVO_WHATSAPP_NUMBER?: string;
 }
 
 interface ImportMeta {
